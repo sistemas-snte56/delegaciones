63858bcbfed2a7bc0839e8a9883ea73521b5af41
sistemas-snte56 <computo.seccional@outlook.com>
2024-05-20 14:57:32 -0600

    Mensaje

---
+++ app/Http/Controllers/RegionController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Region;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+class RegionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $regiones = Region::all();
+
+        return view('config.regiones.index', compact('regiones'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // return "Crear una nueva Región";
+        return view('config.regiones.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Definiendo la regla de validación
+        $validation = $request->validate([
+            'region' => 'required|string',
+            'sede' => 'required|string',
+        ]);
+
+        $region = new Region();
+        $region->region = strtoupper($request->input('region')) ;
+        $region->sede = strtoupper($request->input('sede'));
+        $region->save();
+
+        return redirect('/regiones')->with('success', 'Su registro se guardo con éxito.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Region $region)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Region $region, $id)
+    {
+        $region = Region::find($id);
+        return view('config.regiones.edit', compact('region'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Region $region, $id)
+    {
+
+        $region = Region::find($id);
+
+        // Definiendo la regla de validación
+        $validation = $request->validate([
+            'region' => 'required|string',
+            'sede' => 'required|string',
+        ]);
+
+
+        $region->region = strtoupper($request->input('region')) ;
+        $region->sede = strtoupper($request->input('sede'));
+        $region->save();
+
+        return redirect('/regiones')->with('update', 'Su registro ha sido actualizdo con éxito.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Region $region, $id)
+    {
+        // $region = Region::find($id);
+        
+        // if(!$region) {
+        //     return abort(404);
+        // }
+
+        // $region->delete();
+
+        // return back()->with('delete', 'Su registro ha sido eliminado.');
+        
+        try {
+            $region = Region::find($id);
+            $region->delete();
+            return back()->with('delete', 'Su registro ha sido eliminado.');
+        } catch (QueryException $e) {
+            if ($e->getCode() == 23000) {
+                // return response()->json(['message' => 'No es posible borrar el registro debido a que corresponde a otras delegaciones'], 422);
+                return back()->with('error', 'Error al eliminar la región.');
+            } else {
+                // Si es otra excepción, puedes manejarla según tus necesidades
+                // return response()->json(['message' => 'Error desconocido al eliminar el registro'], 500);
+                return back()->with('error', 'Error desconocido al eliminar el registro.');
+            }
+        }
+    }
+}
---
+++ resources/views/config/regiones/index.blade.php
@@ -0,0 +1,198 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>ADMINISTRACIÓN DE REGIONES</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            Lista de <strong>Regiones</strong>
+
+
+            <a href="{{route('region.create')}}" class="btn btn-primary float-right"><i class="fa fa-sm fa-fw fa-pen"></i> Nuevo</a>
+
+        </div>
+
+        <div class="card-body">
+
+            {{-- Setup data for datatables --}}
+            @php
+                $nlista = 1;
+                $heads = ['ID', 'REGIÓN', 'SEDE', ['label' => 'Actions', 'no-export' => true, 'width' => 15]];
+
+                $btnEdit = '';
+                $btnDelete = '<button type="submit" class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
+                                <i class="fa fa-lg fa-fw fa-trash"></i>
+                            </button>';
+                $btnDetails = '<button class="btn btn-xs btn-default text-teal mx-1 shadow" title="Details">
+                                <i class="fa fa-lg fa-fw fa-eye"></i>
+                            </button>';
+
+                $config = [
+                    'columns' => [
+                        ['type' => 'num'], 
+                        ['orderable' => true], 
+                        ['searchable' => true], 
+                        ['orderable' => true]
+                    ],
+                    // 'columns' => [
+                    //         // ['label' => 'ID'],
+                    //         // ['name' => 'id'],
+                    //         // ['orderable' => true],
+                    //         // ['searchable' => true],
+                    //         // ['type' => 'num'],
+                    //     ],
+                    'language' => [
+                        'url' => '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json',
+                    ],
+                    // 'lengthMenu' => [50,100,500],
+                    'searching' => false,
+                    'paging' => false,
+                    'info' => false,
+
+                ];
+
+            @endphp
+
+            {{-- Minimal example / fill data using the component slot --}}
+            <x-adminlte-datatable id="table1" :heads="$heads" :config="$config"  striped hoverable bordered compressed with-buttons>
+                @foreach ($regiones as $region)
+                    <tr>
+                        <td>{{ $nlista++ }}</td>
+                        <td>{{ $region->region }}</td>
+                        <td>{{ $region->sede }}</td>
+                        <td>
+                            <a href="{{route('region.edit', $region)}}" class="btn btn-xs btn-default text-primary mx-1 shadow" title="Edit">
+                                <i class="fa fa-lg fa-fw fa-pen"></i>
+                            </a>
+                            <form action="{{ route('region.destroy', $region) }}" method="post" class="formEliminar" style="display: inline">
+                                @csrf
+                                @method('DELETE')
+                                {!! $btnDelete !!}
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </x-adminlte-datatable>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+            $('.formEliminar').submit(function(e) {
+                e.preventDefault();
+
+                Swal.fire({
+                    title: "Estas seguro?",
+                    text: "¡No podrás revertir esto!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Si, borrarlo!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        this.submit();
+                    }
+                });
+
+            })
+        });
+    </script>
+
+    @if(session('update'))
+        <script>
+            $(document).ready(function(){
+                let mensaje = "{{ session ('update') }}"
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: mensaje,
+                    showConfirmButton: false,
+                    timer: 1800
+                });
+            });
+        </script>
+    @endif
+
+    @if(session('delete'))
+        <script>
+            $(document).ready(function(){
+                let mensaje = "{{session('delete')}}"
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: mensaje,
+                    showConfirmButton: false,
+                    timer: 1800
+                });
+
+                // Swal.fire({
+                //     title: '¿Estás seguro?',
+                //     text: 'Eliminar esta región también eliminará todas las delegaciones asociadas. ¿Deseas continuar?',
+                //     icon: 'warning',
+                //     showCancelButton: true,
+                //     confirmButtonColor: '#d33',
+                //     cancelButtonColor: '#3085d6',
+                //     confirmButtonText: 'Sí, eliminar',
+                //     cancelButtonText: 'Cancelar'
+                // }).then((result) => {
+                //     if (result.isConfirmed) {
+                //         // Realiza la acción de eliminación
+                //         axios.delete(botonEliminar.href)
+                //             .then(response => {
+                //                 Swal.fire('Éxito', response.data.mensaje, 'success');
+                //             })
+                //             .catch(error => {
+                //                 Swal.fire('Error', error.response.data.mensaje, 'error');
+                //             });
+                //     }
+                // });                
+
+
+
+
+            });
+        </script>
+    @endif    
+
+    @if (session('error'))
+        <script>
+            $(document).ready(function(){
+                let mensaje = "{{session('error')}}"
+                Swal.fire({
+                    icon: "error",
+                    title: mensaje,
+                    text: "Debido a que depende de otras delegaciones. "
+                });
+            });            
+        </script>
+    @endif
+
+
+
+    @if(session('success'))
+        <script>
+            $(document).ready(function(e){
+                let mensaje = "{{ session('success') }}"
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: mensaje,
+                    showConfirmButton: false,
+                    timer: 1800
+                });
+            });
+        </script>
+    @endif
+
+@stop
